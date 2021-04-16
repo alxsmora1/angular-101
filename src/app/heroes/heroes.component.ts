30cc585bc053bddd33b95e958198e729b7f30ab9
Alexis Mora <alexis.mora1v@gmail.com>
2021-04-16 15:15:09 -0500

    Create heroes component and add some logic
    Create the component heroes troughout angular CLI.
    Add the logic for show a list of heroes from a mock of objects.
    Add a simple view on the template file.
    Add styles for the list of heroes and the template.

---
+++ src/app/heroes/heroes.component.ts
@@ -0,0 +1,29 @@
+import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
+import { HEROES } from '../mock-heroes';
+
+@Component({
+  selector: 'app-heroes',
+  templateUrl: './heroes.component.html',
+  styleUrls: ['./heroes.component.css']
+})
+export class HeroesComponent implements OnInit {
+
+  constructor() { }
+
+  heroes = HEROES;
+
+  hero: Hero = {
+    id: 1,
+    name: 'Windstorm'
+  };
+
+  selectedHero?: Hero;
+  onSelect(hero: Hero): void {
+    this.selectedHero = hero;
+  }
+
+  ngOnInit(): void {
+  }
+
+}
