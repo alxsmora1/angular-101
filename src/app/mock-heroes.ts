44c6615138a13f91256bdda5caf80ebd86d33d47
Alexis Mora <alexis.mora1v@gmail.com>
2021-04-16 15:20:48 -0500

    Create the hero mock

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,15 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  { id: 11, name: 'Deku' },
+  { id: 12, name: 'Dynamite' },
+  { id: 13, name: 'Shoto' },
+  { id: 14, name: 'Uravity' },
+  { id: 15, name: 'Ingenium' },
+  { id: 16, name: 'Froppy' },
+  { id: 18, name: 'Endeavor' },
+  { id: 19, name: 'Hawks' },
+  { id: 20, name: 'Best Jeanist' },
+  { id: 21, name: 'Crimson Riot' },
+  { id: 22, name: 'All Might' }
+];
